da9458047ed3dd9de7721fa7027855219524ab8b
Nathan Genetzky <nathan@genetzky.us>
2020-05-27 23:12:52 -0500

    tools: Add script used to setup submodules

---
+++ tools/submodules.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -eux
+
+# yoctoproject
+#
+# https://wiki.yoctoproject.org/wiki/Releases
+# Codename 	Yocto Project Version 	Release Date 	Current Version 	Support Level 	Poky Version 	BitBake branch
+# Thud 	2.6 	Nov 2018 	2.6.4 	Community 	20.0 	1.40
+
+# git submodule add \
+#     --branch '1.40' \
+#     'https://git.openembedded.org/bitbake' \
+#     'submodules/yoctoproject/bitbake'
+# git commit -m 'yoctoproject: thud: Add bitbake'
+
+# git submodule add \
+#     --branch 'thud' --force \
+#     'https://git.yoctoproject.org/git/yocto-autobuilder-helper' \
+#     'submodules/yoctoproject/yocto-autobuilder-helper'
+# git commit -m 'yoctoproject: thud: Add autobuilder helper'
+
+# git submodule add \
+#     --branch '20.0' \
+#     'https://git.yoctoproject.org/git/poky'
+#     'submodules/yoctoproject/poky'
+# git commit -m 'yoctoproject: thud: Add poky'
+
+# git submodule add \
+#     --branch 'poky/thud/qemux86-64' \
+#     'https://git.yoctoproject.org/git/poky-buildhistory'
+#     'submodules/yoctoproject/poky-buildhistory'
+# git commit -m 'yoctoproject: thud: Add poky-buildhistory for qemux86-64'
